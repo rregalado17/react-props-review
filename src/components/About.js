3281388f7fbfda9e527618847457af4a811bef6e
rregalado17 <rregalado17@gmail.com>
2021-07-25 20:56:42 -0400

    react props review

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,47 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About(props) {
+    
+  // const userbio = user.bio
+
+  // return (
+  //   <div id="about">
+  //     <h2>About Me</h2>
+  //     {userbio === null ? null : <p>{userbio}</p>}
+  //     <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+  //     <Links github="https://github.com/liza" linkedin="https://www.linkedin.com/in/liza/"/>
+  //   </div>
+  // );
+  
+  
+  if (!props.bio) {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github="https://github.com/liza" linkedin="https://www.linkedin.com/in/liza/"/>
+      </div>
+    );
+  } else {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{user.bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={user.links.github} linkedin={user.links.linkedin}/>
+      </div>
+    );
+  }
+  // return (
+  //   <div id="about">
+  //     <h2>About Me</h2>
+  //     {/* <p>Put the bio in here</p> */}
+  //     <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+  //     <Links github="https://github.com/liza" linkedin="https://www.linkedin.com/in/liza/"/>
+  //   </div>
+  // );
 }
 
 export default About;
